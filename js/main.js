92de2134a38965f41c3581f896ad597d0c120cc8
bryantee <bryantee@gmail.com>
2015-06-15 16:23:24 -0700

    updateQuestion() function. Needs cleaning up.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+	updateQuestion();
+
 	$('button').on('click', function() {
 		evaluateAnswer();
 		updateBases();
@@ -34,8 +36,8 @@ var Question = function (questionNumber, question, answer1, answer2, answer3, an
 
 // Instantiated objects
 
-var question1 = new Question(1, 'Example Question', 'Wrong Answer', 'Wrong Answer', 'Wrong Answer', 'Correct Answer', 4);
-var question2 = new Question(2, 'Example Question', 'Wrong Answer', 'Wrong Answer', 'Wrong Answer', 'Correct Answer', 4);
+var question1 = new Question(1, 'Who has played the most consecutive games of baseball?', 'Cal Ripken, Jr.', 'Barry Bonds', 'Lou Gehrig', 'Ken Griffey', 4);
+var question2 = new Question(2, 'Example Question', 'Wrong Answer', 'Wrong Answer', 'Wrong Answer', 'Maybe...', 4);
 var question3 = new Question(3, 'Example Question', 'Wrong Answer', 'Wrong Answer', 'Wrong Answer', 'Correct Answer', 4);
 var question4 = new Question(4, 'Example Question', 'Wrong Answer', 'Wrong Answer', 'Wrong Answer', 'Correct Answer', 4);
 var question5 = new Question(5, 'Example Question', 'Wrong Answer', 'Wrong Answer', 'Wrong Answer', 'Correct Answer', 4);
@@ -95,4 +97,28 @@ function updateBases () {
 		default:
 			image.attr("src", "images/field.png");
 	}
+};
+
+function updateQuestion () {
+	// updates question material on page with quesitons and answers
+
+	// setup some variables to make it easier to use
+	var label1 = $('#label1');
+	var label2 = $('#label2');
+	var label3 = $('#label3');
+	var label4 = $('#label4');
+	var qmsg = $('.question');
+	var qcount = $('.question-count'); 
+
+	// append count
+	qcount.empty().append(questionCount + 1);
+
+	//append actualy question
+	qmsg.empty().append(questionsArray[questionCount].question);
+
+	// and the possible answers
+	label1.empty().append(questionsArray[questionCount].answers[0]);
+	label2.empty().append(questionsArray[questionCount].answers[1]);
+	label3.empty().append(questionsArray[questionCount].answers[2]);
+	label4.empty().append(questionsArray[questionCount].answers[3]);
 };
